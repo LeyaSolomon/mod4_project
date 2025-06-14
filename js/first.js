b636beff5cbc1ee6a7ba50949adf4337e356b050
LeyaSolomon <Leyasolomonova@gmail.com>
2025-06-15 02:40:56 +0300

    k

--- js/first.js
+++ js/first.js
@@ -2,6 +2,11 @@
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
     const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
+    const img5 = document.getElementById('img5');
+    const img6 = document.getElementById('img6');
+    const img7 = document.getElementById('img7');
+    const img8 = document.getElementById('img8');
 
     img1.addEventListener('click', function() {
         img1.classList.remove('visible');
@@ -12,4 +17,34 @@
         img2.classList.remove('visible');
         img3.classList.add('visible');
     });
+
+    img3.addEventListener('click', function() {
+        img3.classList.remove('visible');
+        img4.classList.add('visible');
     });
+
+    img4.addEventListener('click', function() {
+        img4.classList.remove('visible');
+        img5.classList.add('visible');
+    });
+
+    img4.addEventListener('click', function() {
+        img4.classList.remove('visible');
+        img5.classList.add('visible');
+    });
+
+    img5.addEventListener('click', function() {
+        img5.classList.remove('visible');
+        img6.classList.add('visible');
+    });
+
+    img6.addEventListener('click', function() {
+        img6.classList.remove('visible');
+        img7.classList.add('visible');
+    });
+
+    img7.addEventListener('click', function() {
+        img7.classList.remove('visible');
+        img8.classList.add('visible');
+    });
+});
